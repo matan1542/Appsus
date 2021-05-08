e9943360116caadbd0a183349dd82c509f75adb0
matan1542 <matan8380@gmail.com>
2021-05-08 17:14:04 +0300

    map

--- apps/note/cmps/note-cmps/IFrame.jsx
+++ apps/note/cmps/note-cmps/IFrame.jsx
@@ -9,7 +9,7 @@ export const Iframe = ({ source }) => {
         // basic bootstrap classes. you can change with yours.
         <div className="col-md-12">
             <div className="emdeb-responsive">
-                <iframe style={{width: "100%"}} src={`http://www.google.com/maps/embed/v1/place?key=AIzaSyChfnf_nR2vBdt8aP4tRoPnwMSuYzriSRU
+                <iframe style={{width: "100%"}} src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyChfnf_nR2vBdt8aP4tRoPnwMSuYzriSRU
         &q=${source}&zoom=18`}></iframe>
             </div>
         </div>
